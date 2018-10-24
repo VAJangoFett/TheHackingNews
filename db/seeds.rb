d3d3bc01b80f621e21ec55e2ef810ac404605765
Jango <arthur.vouloir@gmail.com>
2018-10-24 18:56:42 +0200

    initialize seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  user = User.create!(name: Faker::Hobbit.character)
+end
+
+15.times do
+  link = Link.create!(name: Faker::Internet.domain_name, user_id: rand(1..5))
+end
+
+15.times do
+  comment = Comment.create!(content: Faker::HowIMetYourMother.quote, user_id: rand(1..5), link_id: rand(1..15))
+end
+
+15.times do
+  critic = Critic.create!(content: Faker::HowIMetYourMother.quote, user_id: rand(1..5), comment_id: rand(1..15))
+end
